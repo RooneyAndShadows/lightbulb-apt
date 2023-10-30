75ebe98a4513e5c9c6ffb6d2ea841360c83581e4
rooney123777 <rooney123777@gmail.com>
2023-10-30 10:40:06 +0200

    .

--- lb-plugin/src/main/java/com/github/rooneyandshadows/lightbulb/annotation_processors/plugin/tasks/SourceSetOutput.kt
+++ lb-plugin/src/main/java/com/github/rooneyandshadows/lightbulb/annotation_processors/plugin/tasks/SourceSetOutput.kt
@@ -2,26 +2,34 @@ package com.github.rooneyandshadows.lightbulb.annotation_processors.plugin.tasks
 
 import org.gradle.api.Project
 import org.gradle.api.file.FileCollection
+import org.gradle.api.model.ObjectFactory
 import org.gradle.api.tasks.SourceSet
+import org.gradle.api.tasks.SourceSetContainer
 import java.io.File
 
 class SourceSetOutput(
     private val project: Project,
     private val sourceSet: SourceSet,
 ) {
+    val name: String = sourceSet.name
     val outputCollections: List<FileCollection>
-        get() {
-            val mergedFileCollection = listOf(sourceSet.output.classesDirs, sourceSet.output.generatedSourcesDirs)
-            val outputFileCollections: MutableList<FileCollection> = mutableListOf()
-            mergedFileCollection.forEach { fileCollection ->
-                fileCollection.forEach { dir ->
-                    outputFileCollections.add(project.files(dir))
-                }
-            }
-            return outputFileCollections
-        }
     val classFiles: List<File>
-        get() = outputCollections.map {
+
+    init {
+        val targetOutputs = listOf(sourceSet.output.classesDirs, sourceSet.output.generatedSourcesDirs)
+        outputCollections = targetOutputs.flatMap { it.files }.map { project.files(it) }
+        classFiles = outputCollections.map {
             it.asFileTree.filter { file -> file.extension == "class" }.toList()
         }.flatten()
+    }
+
+    companion object {
+        fun from(project: Project): List<SourceSetOutput> {
+            return project.extensions.getByType(SourceSetContainer::class.java).map { SourceSetOutput(project, it) }
+        }
+
+        fun classFiles(vararg outputs: SourceSetOutput): List<File> {
+
+        }
+    }
 }
--- lb-plugin/src/main/java/com/github/rooneyandshadows/lightbulb/annotation_processors/plugin/tasks/Transformations.kt
+++ lb-plugin/src/main/java/com/github/rooneyandshadows/lightbulb/annotation_processors/plugin/tasks/Transformations.kt
@@ -4,27 +4,20 @@ import com.github.rooneyandshadows.lightbulb.annotation_processors.plugin.transf
 import com.github.rooneyandshadows.lightbulb.annotation_processors.plugin.transformation.Transformation
 import org.gradle.api.Project
 import org.gradle.api.file.FileCollection
-import org.gradle.api.tasks.SourceSetContainer
 import java.io.File
 import java.nio.file.Paths
 
 class Transformations(private val project: Project) {
     private val buildDir: String = project.buildDir.toString()
     private val destinationRoot: String = "transformations"
+    private val sourceSetOutputs: List<SourceSetOutput> = SourceSetOutput.from(project)
     val rootDestinationDir: String = Paths.get(buildDir, destinationRoot).toString()
-    private val sourceSetOutputs: List<SourceSetOutput>
     val classFiles: List<File>
         get() = sourceSetOutputs.map { return@map it.classFiles }.flatten()
     val outputFileCollections: List<FileCollection>
         get() = sourceSetOutputs.map { return@map it.outputCollections }.flatten()
 
     private val transformations: MutableList<Transformation> = mutableListOf()
-
-    init {
-        val sourceSetContainer: SourceSetContainer = project.extensions.getByType(SourceSetContainer::class.java)
-        val transformations = sourceSetContainer.map { return@map SourceSetOutput(project, it) }
-        sourceSetOutputs = transformations
-    }
 
     fun register(transformation: IClassTransformer) {
         transformations.add(Transformation(buildDir, rootDestinationDir, outputFileCollections, transformation))
